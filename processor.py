e9564db74755293527032f312fc341a53f09bf0e
K Eswar Ashish <82705272+eswarashish@users.noreply.github.com>
2024-11-21 13:58:58 +0530

    Update processor.py

--- processor.py
+++ processor.py
@@ -3,9 +3,9 @@ from PIL import Image
 import base64
 from io import BytesIO
 
-def pre_process(input):
+def pre_process(data):
 
-    image_data = base64.b64decode(input)
+    image_data = base64.b64decode(data)
     # Open the image using PIL
     original_image = Image.open(BytesIO(image_data)).convert("L")
     resized_image = original_image.resize((8, 8))
@@ -15,8 +15,8 @@ def pre_process(input):
     return feature_vector
 
 # Post-process function: Converts model predictions to human-readable format
-def post_process(input):
-    predictions=input
+def post_process(data):
+    predictions=data
     label_map = {i: str(i) for i in range(10)}  # Map digits to string labels
     readable_label = label_map[predictions[0]]
     return f"Predicted Digit: {readable_label}"
